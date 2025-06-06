ab515fedfc0b68ad66baa5692f621c16c0937dde
Miwacer <il>
2025-06-07 00:12:35 +0300

    'solution'

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,21 @@
-# write urls here
+from django.urls import path
+from rest_framework import routers
+
+from .views import (
+    GenreViewSet,
+    ActorViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+    CinemaHallViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie-sessions", MovieSessionViewSet)
+router.register("cinema-halls", CinemaHallViewSet)
+
+app_name = "cinema"
+
+urlpatterns = router.urls
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,52 @@
-# write views here
+from django.contrib.admin import action
+from rest_framework import viewsets
+
+from .models import (
+    Genre,
+    Actor,
+    Movie,
+    MovieSession,
+    CinemaHall,
+)
+
+from .serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    CinemaHallSerializer,
+    MovieDetailSerializer,
+    MovieListSerializer,
+    MovieSessionListSerializer, MovieSessionDetailSerializer,
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        return MovieDetailSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionListSerializer
+        return MovieSessionDetailSerializer
